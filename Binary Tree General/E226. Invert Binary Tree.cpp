d8d358ebf732795401f3157714a2572d20656742
Shohag Faraji <shohagfaraji2@gmail.com>
2026-06-22 15:05:03 +0600

    solve LC-226. Invert Binary Tree

---
+++ Binary Tree General/E226. Invert Binary Tree.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/invert-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+
+// Runtime Beats: 100.00%        Memory Beats: 87.36%
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr)
+            return root;
+
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
